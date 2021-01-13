a370236942d0c45af50803d4f75494dced68be5f
Jessica B <jessica.k.bonnie@gmail.com>
2021-01-13 17:50:49 -0500

    updating enrichment code for afr samples and markdown presentation of same

---
+++ code/analysis/african_enrichment.R
@@ -0,0 +1,28 @@
+source('./enrichment.R')
+
+thresholds <- sort(unique(c(seq(1,2,.2), seq(1,5,.5))))
+rootdir <- '/scratch/groups/abattle4/victor/WatershedAFR'
+datadir <- paste0(rootdir,'/','data/')
+
+# African
+
+
+
+
+# files needed for african variants
+afr_z_scores_file = '/scratch/groups/abattle4/victor/WatershedAFR/data/outlier_calling/gtexV8.outlier.controls.v8ciseQTLs.globalOutliers.removed.medz.txt'
+afr_rare_var_pairs_file = '/scratch/groups/abattle4/victor/WatershedAFR/data/rare_variants/gene-AFR-rv.txt' # table of gene-individual pairs with their rare variants
+afr_pop_list_file = '/scratch/groups/abattle4/victor/WatershedAFR/data/data_prep/gtex_v8_wgs_individuals_AFR.txt'
+
+
+afr.output <- enrichmentTipTail(pop_subset_file = afr_pop_list_file,
+                                z_scores_file = afr_z_scores_file,
+                                rare_var_pairs_file = afr_rare_var_pairs_file,
+                                output_dir = datadir,
+                                title.mod.pop = "African",
+                                thresholds = thresholds)
+
+
+
+saveRDS(afr.output, file=paste0(datadir,'african_1to5.rds'))
+
