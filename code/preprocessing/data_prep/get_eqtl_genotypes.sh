14ee1cf2451cbef793e2e349e99089fafb743830
Jessica B <jessica.k.bonnie@gmail.com>
2021-07-30 15:00:23 -0400

    additional preparations to remove hardcoding from get_eqtl_genotypes

--- code/preprocessing/data_prep/get_eqtl_genotypes.sh
+++ code/preprocessing/data_prep/get_eqtl_genotypes.sh
@@ -1,7 +1,38 @@
 #!/bin/bash
 
+
+set -o nounset -o errexit -o pipefail
+
+## Create eQTL genotype files
+## Extract regions with the top eQTLs for each gene in each tissue.
+
+while getopts b:e:c:g:t: flag
+do
+  case "${flag}" in
+    b) eQTL_bed=${OPTARG};;
+    g) gtex_v8_eqtl_dir=${OPTARG};;
+    c) covariates=${OPTARG};;
+    g) eQTL_geno=${OPTARG};;
+    t) TMPDIR=${OPTARG};;
+  esac
+done
+
+# usage() {
+#     cat <<EOM
+#     Usage:
+#     $(basename $0) Explain options here
+# 
+# EOM
+#     exit 0
+# }
+# 
+# [ -z $1 ] && { usage; }
+
 rawdir='/scratch/groups/abattle4/victor/WatershedAFR/raw_data'
 datadir='/scratch/groups/abattle4/victor/WatershedAFR/data'
+eQTL_bed=${datadir}/data_prep/gtex_2017-06-05_v8_cis_eQTLs.bed
+GTEX_WGSv8=${rawdir}/GTEx/GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.SHAPEIT2_phased.vcf.gz
+
 
 ### Check if vcftools and bedtools are available. If not, exit the script
 if ! command -v vcftools &> /dev/null
@@ -17,10 +48,17 @@ then
 fi
 
 ### Generate list of top eQTLs for each gene in each tissue
-eQTL_bed=${datadir}/data_prep/gtex_2017-06-05_v8_cis_eQTLs.bed
-
+# unzip all egenes files
+# keep only fields 14-17
+# keep only lines *not* containing variant_pos
+# replace X chromosome language with 23
+# reprint each line with fields: gene, start position, end position
+# sort by first the gene, then by the start
+# replace 23 chromosome language with X
+# keep only unique lines
 zcat ${rawdir}/GTEx/GTEx_Analysis_v8_eQTL/*.v8.egenes.txt.gz | \
-        cut -f14-17 | grep -v variant_pos | sed 's/^X/23/g' | awk 'BEGIN{OFS="\t"}{print $1,$2-1,$2,$3,$4}' | sort -k 1,1 -k2,2n | \
+        cut -f14-17 | grep -v variant_pos | sed 's/^X/23/g' | \
+        awk 'BEGIN{OFS="\t"}{print $1,$2-1,$2,$3,$4}' | sort -k 1,1 -k2,2n | \
         sed 's/^23/X/g' | uniq > $eQTL_bed
 
 wait
