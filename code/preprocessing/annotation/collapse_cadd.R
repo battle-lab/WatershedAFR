12908c9824d02eb83ea4b765405549e9c2ca5543
Victor Wang <xwang145@jhu.edu>
2022-02-21 19:27:20 -0500

    Imputation of missing values

--- code/preprocessing/annotation/collapse_cadd.R
+++ code/preprocessing/annotation/collapse_cadd.R
@@ -5,6 +5,7 @@
 
 library(data.table)
 library(dplyr)
+library(tidyr)
 
 rv.file = '/scratch/groups/abattle4/victor/WatershedAFR/data/rare_variants_gnomad/gene-AFR-rv.txt'
 out.file ='/scratch/groups/abattle4/victor/WatershedAFR/data/annotation/gene-AFR-rv.CADD.collapse.tsv'
@@ -17,7 +18,7 @@ for (chrom in 1:22){
     rename("Chrom" = "#Chrom") %>%
     mutate(Chrom = paste0("chr",Chrom))
   
-  # Convert genomic coordinates in vep.loftee to be consistent with rare variants (0-based)
+  # Convert genomic coordinates in CADD annotations to be consistent with rare variants (0-based)
   cadd$Pos = cadd$Pos - 1
   # Match rare variants with their annotations and rename columns
   cadd.collapse = filter(rv, Chrom == paste0("chr",chrom)) %>%
@@ -27,7 +28,9 @@ for (chrom in 1:22){
 
 # Gene-Ind level transformation
 cadd.collapse = cadd.collapse %>% group_by(SubjectID, GeneName) %>%
-  summarise(SIFTcat_deleterious=max(SIFTcat_deleterious),
+  summarise(GC=max(GC),
+            CpG=max(CpG),
+            SIFTcat_deleterious=max(SIFTcat_deleterious),
             SIFTcat_tolerated=max(SIFTcat_tolerated), 
             SIFTval=max(SIFTval),
             PolyPhenCat_benign=max(PolyPhenCat_benign),
@@ -43,11 +46,32 @@ cadd.collapse = cadd.collapse %>% group_by(SubjectID, GeneName) %>%
             verPhyloP=max(verPhyloP),
             GerpN=max(GerpN),
             GerpS=max(GerpS),
-            RawScore=max(RawScore)) %>%
+            PHRED=max(PHRED)) %>%
   select(SubjectID, everything(.))
-  # relocate("SubjectID")
 
-#outfile = paste0(tools::file_path_sans_ext(vep.loftee.file), '.collapse.tsv')
+# Impute missing values
+cadd.collapse = cadd.collapse %>%
+  mutate(GC=replace_na(GC, 0.418),
+         CpG=replace_na(CpG, 0.024),
+         SIFTcat_deleterious=replace_na(SIFTcat_deleterious,0),
+         SIFTcat_tolerated=replace_na(SIFTcat_tolerated,0),
+         SIFTval=replace_na(SIFTval, 0),
+         PolyPhenCat_benign=replace_na(PolyPhenCat_benign, 0),
+         PolyPhenCat_possibly_damaging=replace_na(PolyPhenCat_possibly_damaging, 0),
+         PolyPhenCat_unknown=replace_na(PolyPhenCat_unknown, 0),
+         PolyPhenVal=replace_na(PolyPhenVal,0),
+         bStatistic=replace_na(bStatistic, 800.261),
+         priPhCons=replace_na(priPhCons, 0.115),
+         mamPhCons=replace_na(mamPhCons, 0.079),
+         verPhCons=replace_na(verPhCons, 0.094),
+         priPhyloP=replace_na(priPhyloP, -0.033),
+         mamPhyloP=replace_na(mamPhyloP, -0.038),
+         verPhyloP=replace_na(verPhyloP, 0.017),
+         GerpN=replace_na(GerpN, 1.909),
+         GerpS=replace_na(GerpS, -0.2),
+         PHRED=replace_na(PHRED, 0))
+         
+
 fwrite(cadd.collapse, out.file, quote = F, sep = '\t', row.names = F, col.names = header, append=!header)
 header = FALSE
 }
